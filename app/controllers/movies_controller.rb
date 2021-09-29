2351fe27459e09d9ee1a6948282c780742e2d7d0
EC2 Default User <ec2-user@ip-172-31-14-11.us-east-2.compute.internal>
2021-09-29 23:25:57 +0000

    fix try 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,8 +7,7 @@ class MoviesController < ApplicationController
     end
   
     def index
-      @movies = Movie.find(:all, :order => (params[:sort_by]))
-      @sort_column = params[:sort_by]
+      @movies = Movie.all
     end
   
     def new

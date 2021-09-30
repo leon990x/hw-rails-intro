1446ee45c4925bb3453dcc4db125afc02d0efaf6
EC2 Default User <ec2-user@ip-172-31-14-11.us-east-2.compute.internal>
2021-09-30 03:41:45 +0000

    check checkboxes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
     def self.ratings
-        Movie.select(:rating).distinct.inject([]) {|arr, mov| arr.push mov.rating}
+        Movie.select(:rating).distinct.inject([]) {|a, m| a.push m.rating}
     end
 end

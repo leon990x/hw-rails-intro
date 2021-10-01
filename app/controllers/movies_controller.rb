cdf03a1bcaa9dd547100f06bd4aedb9b87e431d2
EC2 Default User <ec2-user@ip-172-31-14-11.us-east-2.compute.internal>
2021-10-01 04:34:50 +0000

    plzzzz

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,11 +8,26 @@ class MoviesController < ApplicationController
   
     def index
       #@movies = Movie.all
-      @all_ratings = Movie.ratings
       @sort = params[:sort]
-      params[:ratings].nil? ? @t_param = @all_ratings : @t_param = params[:ratings].keys
-      @movies = Movie.where(rating: @t_param).order(@sort)
-
+      @movies = Movie.all.order(@sort)
+      @all_ratings = ['G','PG','PG-13','R']
+      session[:ratings] = params[:ratings] unless params[:ratings].nil?
+      session[:order] = params[:order] unless params[:order].nil?
+  
+      if (params[:ratings].nil? && !session[:ratings].nil?) || (params[:order].nil? && !session[:order].nil?)
+        redirect_to movies_path("ratings" => session[:ratings], "order" => session[:order])
+      elsif !params[:ratings].nil? || !params[:order].nil?
+        if !params[:ratings].nil?
+          array_ratings = params[:ratings].keys
+          return @movies = Movie.where(rating: array_ratings).order(session[:order])
+        else
+          return @movies = Movie.all.order(session[:order])
+        end
+      elsif !session[:ratings].nil? || !session[:order].nil?
+        redirect_to movies_path("ratings" => session[:ratings], "order" => session[:order])
+      else
+        return @movies = Movie.all
+      end
     end
   
     def new

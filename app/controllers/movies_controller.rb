9fae33f859dfef0e8936d90a2087cca2f89ffe5d
EC2 Default User <ec2-user@ip-172-31-14-11.us-east-2.compute.internal>
2021-10-02 22:35:10 +0000

    readd method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,17 +7,17 @@ class MoviesController < ApplicationController
     end
   
     def index
-      @all_ratings = Movie.ratings 
-      @ratings = @all_ratings 
-      @ratings =  params[:ratings].keys if params[:ratings] 
-      @movies = Movie.where(rating: @ratings)
-      @sort=params[:sort]
-      @movies = Movie.all.order(@sort)
-      if @sort
-        @movies = @movies.where(rating: @ratings).order(@sort)
-      else
-        @movies = Movie.where(rating: @ratings)
-      end
+      # @all_ratings = Movie.ratings 
+      # @ratings = @all_ratings 
+      # @ratings =  params[:ratings].keys if params[:ratings] 
+      # @movies = Movie.where(rating: @ratings)
+      # @sort=params[:sort]
+      # @movies = Movie.all.order(@sort)
+      # if @sort
+      #   @movies = @movies.where(rating: @ratings).order(@sort)
+      # else
+      #   @movies = Movie.where(rating: @ratings)
+      # end
       
       # session[:ratings] = params[:ratings] if params[:ratings] || params[:commit] == 'Refresh'
       # session[:sort] = params[:sort] if params[:sort]

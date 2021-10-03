d25b62d8c55d2c393c9baf931dfad88ee298febc
EC2 Default User <ec2-user@ip-172-31-14-11.us-east-2.compute.internal>
2021-10-03 18:49:47 +0000

    modelchange

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ActiveRecord::Base
     def self.all_ratings
-        self.all.select(:rating).distinct.pluck(:rating)
+        #self.all.select(:rating).distinct.pluck(:rating)
+        Movie.select(:rating).distinct.inject([]) { |a, m| a.push m.rating }
     end
 end

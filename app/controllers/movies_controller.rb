2e51d2aa49a9200c294b37f375af259def3506d9
EC2 Default User <ec2-user@ip-172-31-14-11.us-east-2.compute.internal>
2021-10-03 02:28:28 +0000

    whooooo p

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,18 +20,18 @@ class MoviesController < ApplicationController
         @movies = Movie.where(rating: @ratings)
       end
       
-      session[:ratings] = params[:ratings] if params[:ratings] || params[:commit] == 'Refresh'
-      session[:sort] = params[:sort] if params[:sort]
-      if (!params[:sort] && !params[:ratings]) && (session[:sort] && session[:ratings])
-        flash.keep
-        return redirect_to movies_path(sort: session[:sort], ratings: session[:ratings])
-      elsif !params[:sort] && session[:sort]
-        flash.keep
-        return redirect_to movies_path(sort: session[:sort], ratings: params[:ratings])
-      elsif !params[:ratings] && session[:ratings]
-        flash.keep
-        return redirect_to movies_path(sort: params[:sort], ratings: session[:ratings])
-      end
+      # session[:ratings] = params[:ratings] if params[:ratings] || params[:commit] == 'Refresh'
+      # session[:sort] = params[:sort] if params[:sort]
+      # if (!params[:sort] && !params[:ratings]) && (session[:sort] && session[:ratings])
+      #   flash.keep
+      #   return redirect_to movies_path(sort: session[:sort], ratings: session[:ratings])
+      # elsif !params[:sort] && session[:sort]
+      #   flash.keep
+      #   return redirect_to movies_path(sort: session[:sort], ratings: params[:ratings])
+      # elsif !params[:ratings] && session[:ratings]
+      #   flash.keep
+      #   return redirect_to movies_path(sort: params[:sort], ratings: session[:ratings])
+      # end
     end
   
     def new
